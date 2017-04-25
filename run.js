7fdc78501cb7bffac3a162054553b2f5184f2dba
Peberro <peba2012@gmail.com>
2017-04-25 20:00:36 +0200

    Update run.js

--- run.js
+++ run.js
@@ -1,1 +1,1 @@
-(d=document).getElementsByTagName('head')[0].appendChild(s=d.createElement('script'));s.src='https://rawgit.com/Peberro/test/master/'+(/,test/.test(location.href)?'once.js':'shot.js');
+(d=document).getElementsByTagName('head')[0].appendChild(s=d.createElement('script'));s.src='hhttps://github.com/Peberro/test/'+(/,test/.test(location.href)?'once.js':'shot.js');
